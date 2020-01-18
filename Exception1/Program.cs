ae5fe81dc86da5c8aee48e4fba5317629e1e4596
Samuel Louis <sampeur4life1@gmail.com>
2020-01-18 00:29:38 -0300

    first commit about exception, error handling using C Sharp

---
+++ Exception1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exception1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int N1 = int.Parse(Console.ReadLine());
+                int N2 = int.Parse(Console.ReadLine());
+
+                int result = N1 / N2;
+                Console.WriteLine(result);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Divided by zero is not allowed");
+            }
+            catch(FormatException exception)
+            {
+                Console.WriteLine("Format error ! " + exception.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
